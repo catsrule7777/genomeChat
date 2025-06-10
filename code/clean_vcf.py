89e35515e279421b50245caf391dca8e7485913c
catsrule7777 <afamily54321@gmail.com>
2025-06-09 20:20:02 -0700

    added clean_vcf

---
+++ code/clean_vcf.py
@@ -0,0 +1,15 @@
+from cyvcf2 import VCF
+import pandas as pd
+
+print('clean_vcf ran')
+
+def clean_vcf(file_path):
+
+    data = []
+    for variant in VCF(file_path):
+        d = {'CHROM' : variant.CHROM, 'POS' : variant.POS, 'ID' : variant.ID, 'REF' : variant.REF, 'ALT' : variant.ALT, 'QUAL' : variant.QUAL, 'FILTER' : variant.FILTER, 'DP' : variant.INFO['DP'], 'FORMAT' : variant.FORMAT}
+        data.append(d)
+
+    df = pd.DataFrame(data)
+
+    df.to_csv(f'{file_path[:len(file_path)-4]}.csv')
---
+++ code/temp.py
@@ -0,0 +1,5 @@
+from clean_vcf import clean_vcf
+
+print('temp.py ran')
+
+clean_vcf('vcf_sample.vcf')
