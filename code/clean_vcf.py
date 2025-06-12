3c4ef1ac76199620751c133211d3207e5e407114
catsrule7777 <afamily54321@gmail.com>
2025-06-11 20:26:48 -0700

    made cleanvcf work for clinvar

--- code/clean_vcf.py
+++ code/clean_vcf.py
@@ -3,13 +3,23 @@ import pandas as pd
 
 print('clean_vcf ran')
 
-def clean_vcf(file_path):
+def clean_vcf(file_path, clinvar=False):
 
-    data = []
-    for variant in VCF(file_path):
-        d = {'CHROM' : variant.CHROM, 'POS' : variant.POS, 'ID' : variant.ID, 'REF' : variant.REF, 'ALT' : variant.ALT, 'QUAL' : variant.QUAL, 'FILTER' : variant.FILTER, 'DP' : variant.INFO['DP'], 'FORMAT' : variant.FORMAT}
-        data.append(d)
+    if not clinvar:
+        data = []
+        for variant in VCF(file_path):
+            d = {'CHROM' : variant.CHROM, 'POS' : variant.POS, 'ID' : variant.ID, 'REF' : variant.REF, 'ALT' : variant.ALT, 'QUAL' : variant.QUAL, 'FILTER' : variant.FILTER, 'DP' : variant.INFO.get('DP', 0), 'FORMAT' : variant.FORMAT}
+            data.append(d)
 
-    df = pd.DataFrame(data)
+        df = pd.DataFrame(data)
 
-    df.to_csv(f'{file_path[:len(file_path)-4]}.csv')
+        df.to_csv(f'{file_path[:len(file_path)-4]}.csv')
+    else:
+        data = []
+        for variant in VCF(file_path):
+            d = {'CHROM' : variant.CHROM, 'POS' : variant.POS, 'REF' : variant.REF, 'ALT' : variant.ALT,'CLNSIG' : variant.INFO.get('CLNSIG', 0)}
+            data.append(d)
+        df = pd.DataFrame(data)
+
+        df.to_csv(f'{file_path[:len(file_path)-4]}.csv')
+
--- code/temp.py
+++ code/temp.py
@@ -2,4 +2,4 @@ from clean_vcf import clean_vcf
 
 print('temp.py ran')
 
-clean_vcf('vcf_sample.vcf')
+clean_vcf('clinvar_sample.vcf', clinvar=True)
