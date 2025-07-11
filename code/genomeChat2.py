4d183f734f9d45d081541714dad357a2bdcfbf8f
catsrule7777 <afamily54321@gmail.com>
2025-07-10 20:42:03 -0700

    updated genomeChat2

--- code/genomeChat2.py
+++ code/genomeChat2.py
@@ -5,21 +5,33 @@ import pandas as pd
 from merge_vcf import merge_vcf
 from clean_vcf import clean_vcf, clean_clinvar
 from langchain import hub
+from dotenv import load_dotenv
+import os 
+from langchain_openai import ChatOpenAI 
 
-
+load_dotenv()
+api_key = os.getenv('OPENAI_KEY')
 
 prompt = hub.pull("hwchase17/react")
 
 tools = [merge_vcf, clean_vcf, clean_clinvar]
-llm = ChatOllama(model='gemma3')
+llm = ChatOllama(model='qwen3')
 
 agent = create_react_agent(llm, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools)
+agent_executor = AgentExecutor(agent=agent, tools=tools, handle_parsing_errors=True)
+
+
 
 st.title('Genome Chat 2')
-query = st.text_input('')
-template = 'Answer in fewer than 100 words, do not use tools unless absolutely needed '
 
-st.write(agent_executor.invoke({'input' : f'{template}{query}'}))
+with st.form('form'):
+    query = st.text_input('')
+    template = 'Answer in fewer than 100 words, do not use tools unless absolutely needed '
+
+    submit_button = st.form_submit_button('Submit')
+
+    if submit_button:
+        
+        st.write(agent_executor.invoke({'input' : f'{template}{query}'}))
     
 
--- code/merge_vcf.py
+++ code/merge_vcf.py
@@ -3,16 +3,21 @@ from langchain.agents import tool
 
 @tool
 def merge_vcf(paths):
-    """a tool that takes in two comma seperated file paths, and returns the merged result of the two files"""
+    """a tool that takes in two comma seperated file paths (csv format), and returns the merged result of the two files"""
     paths = paths.split(',')
-    vcf_path = paths[0].strip(' ')
-    clinvar_path = paths[1].strip(' ')
-    vcf = pd.read_csv(vcf_path)
-    clinvar = pd.read_csv(clinvar_path)
+    vcf_path = paths[0].strip()
+    clinvar_path = paths[1].strip()
+    vcf = pd.read_csv(vcf_path, dtype=str)
+    clinvar = pd.read_csv(clinvar_path, dtype=str)
 
     vcf['CHROM'] = vcf['CHROM'].str.strip('chr')
-    vcf['CHROM'] = vcf['CHROM'].astype(int)
+    vcf['CHROM'] = vcf['CHROM'].astype(str)
     vcf['POS'] = vcf['POS'].astype(int)
+
+    clinvar['CHROM'] = clinvar['CHROM'].str.strip('chr')
+    clinvar['CHROM'] = clinvar['CHROM'].astype(str)
+    clinvar['POS'] = clinvar['POS'].astype(int)
+
     merged = vcf.merge(clinvar, on=['CHROM', 'POS'], how='inner')
 
     merged.to_csv('merged.csv')
